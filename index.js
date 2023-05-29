dbfd1d0dbd906e720b9a76a0319a85060612ecac
Arjun Adhikari <46340446+arjunQ21@users.noreply.github.com>
2023-05-29 15:56:32 +0545

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(8081, function () {
-  console.log('Listening on port 8081...')
+app.listen(process.env.PORT, function () {
+  console.log('Listening on port ...' + process.env.PORT)
 })
